04d2d6856496ce2fb7fbb4698a0f635427d98874
OrangeB0lt <667@holbertonschool.com>
2019-09-04 10:27:35 -0400

    [AirBnB][flask web making][task 5] added conditional statement to task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -33,8 +33,9 @@ def shNumber(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def numTemplate(n):
-    return render_template('5-number.html', n=n)
+def numTemplate(n=None):
+    if isinstance(n, int):
+        return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
